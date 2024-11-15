48269289cd9a50d59c908d2b08625acc0603292b
Behzod <behzodjaloliddinov870@gmail.com>
2024-11-15 13:10:25 +0300

    Initial commit

---
+++ src/test/java/uz/behzod/RestarunantApp/RestarunantAppApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.behzod.RestarunantApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestarunantAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
