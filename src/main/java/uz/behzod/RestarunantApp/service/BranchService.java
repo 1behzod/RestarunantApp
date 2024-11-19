1c53804c32e0e9f6f0156766102c9f29548a9a5c
Behzod <behzodjaloliddinov870@gmail.com>
2024-11-19 12:30:22 +0300

    Optimized

---
+++ src/main/java/uz/behzod/RestarunantApp/service/BranchService.java
@@ -0,0 +1,56 @@
+package uz.behzod.RestarunantApp.service;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import uz.behzod.RestarunantApp.domain.branch.Branch;
+import uz.behzod.RestarunantApp.repository.BranchRepository;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+public class BranchService {
+
+    BranchRepository branchRepository;
+
+    public void validate(Branch branch) {
+
+        if (branch.getName() == null || branch.getName().isEmpty()) {
+            throw new RuntimeException("Name field is required");
+        }
+        if (branch.getCompanyId() == null) {
+            throw new RuntimeException("Branch must be associated with a valid company");
+        }
+        if (branch.getAddress() == null) {
+            throw new RuntimeException("Branch address is required");
+        }
+        if (branch.getId() != null && branchRepository.existsByCompanyId(branch.getCompanyId())) {
+            throw new RuntimeException("Branch already exists");
+        }
+    }
+
+
+    @Transactional
+    public void delete(Branch branch) {
+        if (!branchRepository.existsById(branch.getId())) {
+            throw new RuntimeException("Branch not found");
+        }
+        branchRepository.delete(branch);
+    }
+
+    @Transactional
+    public Branch create(Branch newBranch) {
+        this.validate(newBranch);
+        Branch branch = new Branch();
+        branch.setName(newBranch.getName());
+        branch.setCompanyId(newBranch.getCompanyId());
+        branch.setAddress(newBranch.getAddress());
+        return branchRepository.save(branch);
+
+    }
+
+}
---
+++ src/main/java/uz/behzod/RestarunantApp/service/RegionService.java
@@ -0,0 +1,12 @@
+package uz.behzod.RestarunantApp.service;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+public class RegionService {
+}
